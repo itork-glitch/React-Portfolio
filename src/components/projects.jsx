04fcb5528893784d4ecdd3142e033a0724bd6dfe
itork-glitch <piter09102009sx@gmail.com>
2023-02-05 09:38:00 +0100

    projects 3.2/5

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -28,8 +28,47 @@ export const Projects = ({ id }) => {
             </div>
           </div>
         </div>
-        <div className="card2">G</div>
-        <div className="card3 md:col-[-3/-1] lg:col-[3/4]">H</div>
+        <div className="card2 flex justify-center items-center my-3">
+          {' '}
+          <div className="card lg:h-[50vh] h-[60vh] w-[80%] flex justify-center items-center flex-col">
+            <div className="title -mt-[3vh] lg:-mt-[2vh] font-semibold text-[45px] mb-[45px]">
+              <h1>Fire Bot</h1>
+            </div>
+            <div className="description flex flex-col justify-center">
+              <p className="text-center px-5 text-md mb-[45px]">
+                Fire Bot to bot do discorda o szerokim zastosowaniu posiada dużo
+                komend każdej kategori. <br /> Dowiedz się więcej klikając w
+                link.
+              </p>
+              <button className="mb-[30px] p-4 rounded-xl text-white bg-black w-[60%] self-center hover:scale-110 transition-all duration-300 ease-out btn-shadow">
+                <Link to="/commands">Sprawdź moje komendy</Link>
+              </button>
+              <button className="p-4 rounded-xl text-white bg-black w-[80%] md:w-[60%] self-center hover:scale-110 transition-all duration-300 ease-out btn-shadow">
+                <Link to="/fire">Dowiedz się więcej</Link>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card3 md:col-[-3/-1] lg:col-[3/4] flex justify-center items-center my-3">
+          <div className="card lg:h-[50vh] h-[60vh] w-[80%] md:w-[40%] sm:w-[80%] lg:w-[80%]  flex justify-center items-center flex-col">
+            <div className="title -mt-[3vh] lg:-mt-[2vh] font-semibold text-[45px] mb-[45px]">
+              <h1>Fire Bot</h1>
+            </div>
+            <div className="description flex flex-col justify-center">
+              <p className="text-center px-5 text-md mb-[45px]">
+                Fire Bot to bot do discorda o szerokim zastosowaniu posiada dużo
+                komend każdej kategori. <br /> Dowiedz się więcej klikając w
+                link.
+              </p>
+              <button className="mb-[30px] p-4 rounded-xl text-white bg-black w-[60%] self-center hover:scale-110 transition-all duration-300 ease-out btn-shadow">
+                <Link to="/commands">Sprawdź moje komendy</Link>
+              </button>
+              <button className="p-4 rounded-xl text-white bg-black w-[80%] md:w-[60%] self-center hover:scale-110 transition-all duration-300 ease-out btn-shadow">
+                <Link to="/fire">Dowiedz się więcej</Link>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
