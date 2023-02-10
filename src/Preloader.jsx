5512ce0052ac8eb161365019e0c0b5d2a5948963
itork-glitch <piter09102009sx@gmail.com>
2023-02-10 17:00:42 +0100

    add pre loader

---
+++ src/Preloader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/Preloader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="layer"></div>
+      <div className="layer"></div>
+      <div className="layer"></div>
+    </div>
+  );
+};
+
+export default Loader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import Preloader from './Preloader';
 import App from './App';
 import './global.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -7,12 +8,22 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { BrowserRouter } from 'react-router-dom';
 
+const Root = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3500);
+  }, []);
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>{loading ? <Preloader /> : <App />}</BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
 library.add(fas, fab);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
